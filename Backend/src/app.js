8764ea1de18f0394fd6391c3c930e76ff583dfc2
xander <alramirezba@est.utn.ac.cr>
2026-06-16 10:07:32 -0600

    intalado cors

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,16 +1,25 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
-const app = express();
 
+
+const app = express();
 const NAME = process.env.SERVER_NAME;
 const VERSION = process.env.SERVER_VERSION;
 const DESCRIPTION = process.env.SERVER_DESCRIPTION;
 const PORT = process.env.SERVER_PORT;
 
+app.use(cors());
+app.use(express.json());
+
 app.get("/",(req,res)=>{
-    res.send(`<h1>${NAME} - ${VERSION}</h1><p>${DESCRIPTION}</p>`);
+    res.json({ 
+        name: NAME, 
+        version: VERSION, 
+        description: DESCRIPTION 
+    });
 });
 
 app.listen(PORT,()=>{
