1324044a92010d01d6150ba9dd310dbe8bdb469a
xander <alramirezba@est.utn.ac.cr>
2026-06-16 09:37:35 -0600

    Cambios

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,11 +1,18 @@
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 const app = express();
 
+const NAME = process.env.SERVER_NAME;
+const VERSION = process.env.SERVER_VERSION;
+const DESCRIPTION = process.env.SERVER_DESCRIPTION;
+const PORT = process.env.SERVER_PORT;
 
 app.get("/",(req,res)=>{
-    res.send('<h1>hello world</h1>');
+    res.send(`<h1>${NAME} - ${VERSION}</h1><p>${DESCRIPTION}</p>`);
 });
 
-app.listen(4000,()=>{
-    console.log("servidor en puerto http://localhost:4000");
+app.listen(PORT,()=>{
+    console.log(`${NAME} - ${VERSION} running on http://localhost:${PORT}`);
 });
